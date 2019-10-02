24cc2cb9441e374d2226a249dfd71eb2ec70b4cf
tmb <tom.brooke@gmail.com>
2019-10-01 22:32:32 -0400

    Set up exercises

--- October10.py
+++ October10.py
@@ -45,3 +45,34 @@ def term_grader(grade):
     return grader(term)
 
 print(term_grader(grades))
+
+
+## Note, I cheated with the sum function:
+# 
+# Here is a home grown one
+#
+
+def sum_list(items):
+    sum_numbers = 0
+    for x in items:
+        sum_numbers += x
+    return sum_numbers
+
+def term_grader2(grade):
+    term = sum_list(grade) / len(grade)
+    return grader(term)
+
+print(term_grader2(grades))
+
+# 1. Write a program to multiply the numbers in a list.
+## Hint *= is a thing
+
+# 2. Write a program to get the largest number in a list
+
+# 3. Assume: law_class = ["Matthew", "Hunter", "Marianna", "Brittany", "Harrison", "James","Bradley"]
+# Print "Hunter"
+ 
+# 4. Print out each name in law_class
+
+# 5. Put the law_class in alphabetical order
+
--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ## Reading:
 
+### [How to Build an Interactive Legal Application](https://www.nonprofittechy.com/2019/10/01/basic-elements-of-an-interactive-legal-application/)
+
 ### [Chapter 4 - Lists](https://automatetheboringstuff.com/chapter4/)
 
-## Code:
+## Code exercises:
+
 ### [October10.py](https://github.com/Campbell-law/Oct-10-lists/blob/master/October10.py)
